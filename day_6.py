e0b31e6185710ec1c1bfa8f7db8bb47df3e829ac
Aooddy <constant.eng.cmu@gmail.com>
2022-10-09 14:07:10 +0000

    slider

--- day_6.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-st.title('🎈 App Name')
-st.subheader('ndf')
-
-st.write('Hello nattawut savassook 530610425!')
-
---
+++ st_slider.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from datetime import time,datetime
+
+st.header('st.hearder')
+
+#Example 1
+st.subheader('#Example 1')
+
+age  = st.slider('How old are you?',0,130,25)
+st.write("I'm",age,'years old')
+
+
+#Example 2
+st.subheader('#Example 2 Range slider')
+values = st.slider(
+    'Select a range of values',
+    0.0,100.0,(25.0,75.0)
+)
+st.write('Values',values)
+
+
+#Example 3
+st.subheader('#Example 3 Range time slider')
+appointment = st.slider(
+    "Schedule your appointment: ",
+    value=(time(11,30),time(12,45))
+)
+st.write("You're scheduled for :",appointment)
+
+#Example 4
+st.subheader('#Example 4 Datatime slider')
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020,1,1,9,30),
+    format="MM/DD/YY- hh:mm"
+
+)
+st.write("start_time: ",start_time)
+
+
+
+
+
