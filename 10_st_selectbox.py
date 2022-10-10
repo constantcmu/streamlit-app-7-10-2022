98c05463178bb28c95f271ab8942a884a6e37b55
constantcmu <constant.eng.cmu@gmail.com>
2022-10-10 10:34:34 +0700

    select

--- 10_st_selectbox.py
+++ 10_st_selectbox.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
-st.header("st.selectbox")
+
+#Example 1
+st.header("Example 1 st.selectbox")
 
 option = st.selectbox(
     "what is your favorite color?",
     ("Blue","Red","Green")
 )
 st.write("Your favorite color is : ",option)
+
+#Example 2
+st.header("Example 2 st.multiselect")
+
+option2 = st.multiselect(
+    "What are your favorite colors",
+    ['Green','Yellow','Red','Blue'],
+    ['Yellow','Red']
+)
+st.write("Your selected : ",option2)
+
+
 
 
